01302c44e37fc5f6866121b0a110c6ea9dd1c9d9
Maxime <m.marchand46@laposte.net>
2017-05-23 19:39:54 +0200

    vérifie extension fichier

--- js/script.js
+++ js/script.js
@@ -1,177 +1,193 @@
-function trace_gpx(){
-  var gpx_data;
-  $.ajax('pyramide.gpx').done(function(xml) {
-    gpx_data = toGeoJSON.gpx(xml);
-    console.log(gpx_data);
+function trace_gpx() {
+    var gpx_data;
+    $.ajax('pyramide.gpx').done(function(xml) {
+        gpx_data = toGeoJSON.gpx(xml);
+        console.log(gpx_data);
 
-    var latlngs = [];
+        var latlngs = [];
 
-    for (var i=0; i<gpx_data.features[0].geometry.coordinates.length;i++){
-      lon = gpx_data.features[0].geometry.coordinates[i][0];
-      lat = gpx_data.features[0].geometry.coordinates[i][1];
-      latlngs.push([lat, lon]);
+        for (var i = 0; i < gpx_data.features[0].geometry.coordinates.length; i++) {
+            lon = gpx_data.features[0].geometry.coordinates[i][0];
+            lat = gpx_data.features[0].geometry.coordinates[i][1];
+            latlngs.push([lat, lon]);
+        }
 
-    }
-console.log(latlngs);
-var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
-// zoom the map to the polyline
-polyline.dragging.enable();
-polyline.enableEdit();
+        console.log(latlngs);
+        var polyline = L.polyline(latlngs, {
+            color: 'red'
+        }).addTo(map);
+        // zoom the map to the polyline
+        polyline.dragging.enable();
+        polyline.enableEdit();
 
+        map.fitBounds(polyline.getBounds());
 
-map.fitBounds(polyline.getBounds());
-
-    //var myLayer = L.geoJSON(gpx_data.features,{onEachFeature: onEachFeature}).addTo(map);
-    //myLayer.addData(gpx_data, {draggable: true});
-    //myLayer.dragging.enable();
-    //map.fitBounds(myLayer.target.getBounds());
-  });
+    });
 }
 
 
-function trace_gpx_image(){
-  var gpx_data;
-  L.easyButton('fa-globe', function(btn, map){
-window.open("file:///" );
-  }).addTo(map);
+function trace_gpx_image() {
+    var gpx_data;
+    L.easyButton('fa-globe', function(btn, map) {
+        window.open("file:///");
+    }).addTo(map);
 
-  $.ajax('CO_MD_Le_Senequet.gpx').done(function(xml) {
-    gpx_data = toGeoJSON.gpx(xml);
-    console.log(gpx_data);
+    $.ajax('CO_MD_Le_Senequet.gpx').done(function(xml) {
+        gpx_data = toGeoJSON.gpx(xml);
+        console.log(gpx_data);
 
-    var latlngs = [];
+        var latlngs = [];
 
-    for (var i=0; i<gpx_data.features[0].geometry.coordinates.length;i++){
-      lon = gpx_data.features[0].geometry.coordinates[i][0];
-      lat = gpx_data.features[0].geometry.coordinates[i][1];
-      latlngs.push([lat, lon]);
-    }
-
-
-console.log(latlngs);
-var polyline = L.polyline(latlngs, {color: 'green'}).addTo(map);
-// zoom the map to the polyline
-polyline.dragging.enable();
+        for (var i = 0; i < gpx_data.features[0].geometry.coordinates.length; i++) {
+            lon = gpx_data.features[0].geometry.coordinates[i][0];
+            lat = gpx_data.features[0].geometry.coordinates[i][1];
+            latlngs.push([lat, lon]);
+        }
 
 
-  });
+        console.log(latlngs);
+        var polyline = L.polyline(latlngs, {
+            color: 'green'
+        }).addTo(map);
+        // zoom the map to the polyline
+        polyline.dragging.enable();
+
+
+    });
 
 }
 
-function load_gpx(contents){
-  var gpx_data;
-//console.log(contents);
+function load_gpx(contents) {
+    var gpx_data;
+    //console.log(contents);
     gpx_data = toGeoJSON.gpx(jQuery.parseXML(contents));
-    console.log(gpx_data);
+    //console.log(gpx_data);
 
     var latlngs = [];
 
-    for (var i=0; i<gpx_data.features[0].geometry.coordinates.length;i++){
-      lon = gpx_data.features[0].geometry.coordinates[i][0];
-      lat = gpx_data.features[0].geometry.coordinates[i][1];
-      latlngs.push([lat, lon]);
+    for (var i = 0; i < gpx_data.features[0].geometry.coordinates.length; i++) {
+        lon = gpx_data.features[0].geometry.coordinates[i][0];
+        lat = gpx_data.features[0].geometry.coordinates[i][1];
+        latlngs.push([lat, lon]);
     }
 
-
-console.log(latlngs);
-var polyline = L.polyline(latlngs, {color: 'green'}).addTo(map);
-// zoom the map to the polyline
-polyline.dragging.enable();
+    console.log(latlngs);
+    var polyline = L.polyline(latlngs, {
+        color: 'green'
+    }).addTo(map);
+    // zoom the map to the polyline
+    polyline.dragging.enable();
 
 }
 
 function readGPXFile(e) {
-  var file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    var contents = e.target.result;
-console.log(typeof(contents));
-load_gpx(contents);
-    //displayContents(contents);
-  };
-reader.readAsText(file);
-
-
+    var file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    if(file.name.substring(file.name.lastIndexOf('.'))!=".gpx"){
+      //Alerte sur le format du fichier
+      alert('Choisir un fichier gpx');
+      return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var contents = e.target.result;
+        console.log(typeof(contents));
+        load_gpx(contents);
+        //displayContents(contents);
+    };
+    reader.readAsText(file);
 }
+
 var overlay; //image overlay
 function readImage(e) {
-  var file = e.target.files[0];
-  if (!file) {
-    return;
-  }
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    var contents = e.target.result;
-//console.log(file.name);
-var point1 = L.latLng( 49.083536, -1.607909),
-		    point2 = L.latLng(49.083536, -1.604648),
-		    point3 = L.latLng(49.08719, -1.604583);
+    var file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    if(file.name.substring(file.name.lastIndexOf('.'))!=".jpg" && file.name.substring(file.name.lastIndexOf('.'))!=".png"){
+      //Alerte sur le format du fichier
+      alert('Choisir un fichier jpg ou png');
+      return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var contents = e.target.result;
+        //console.log(file.name);
+        var point1 = L.latLng(49.083536, -1.607909),
+            point2 = L.latLng(49.083536, -1.604648),
+            point3 = L.latLng(49.08719, -1.604583);
 
-		var marker1 = L.marker(point1, {draggable: true} ).addTo(map),
-		    marker2 = L.marker(point2, {draggable: true} ).addTo(map),
-		    marker3 = L.marker(point3, {draggable: true} ).addTo(map);
+        var marker1 = L.marker(point1, {
+                draggable: true
+            }).addTo(map),
+            marker2 = L.marker(point2, {
+                draggable: true
+            }).addTo(map),
+            marker3 = L.marker(point3, {
+                draggable: true
+            }).addTo(map);
 
 
-		var	bounds = new L.LatLngBounds(point1, point2).extend(point3);
-		map.fitBounds(bounds);
-		 overlay = L.imageOverlay.rotated(file.name, point1, point2, point3, {
-			opacity: 0.4,
-			interactive: true
-		});
-
-
-
-		marker1.on('drag dragend', repositionImage);
-		marker2.on('drag dragend', repositionImage);
-		marker3.on('drag dragend', repositionImage);
-
-    var switch_marker = 1;
-		map.addLayer(overlay);
-		overlay.on('dblclick',function (e) {
-			console.log('Double click on image.');
-      //cache les markers
-      if(switch_marker==0){
-
-        marker1 = L.marker(point1, {draggable: true} ).addTo(map);
-    		    marker2 = L.marker(point2, {draggable: true} ).addTo(map);
-    		    marker3 = L.marker(point3, {draggable: true} ).addTo(map);
-            marker1.on('drag dragend', repositionImage);
-        		marker2.on('drag dragend', repositionImage);
-        		marker3.on('drag dragend', repositionImage);
-        switch_marker=1
-      }
-      else{
-        map.removeLayer(marker1);
-        map.removeLayer(marker2);
-        map.removeLayer(marker3);
-        switch_marker =0;
-      }
-
-			//e.stop();
-		});
-		overlay.on('click',function (e) {
-			console.log('Click on image.');
-		});
+        var bounds = new L.LatLngBounds(point1, point2).extend(point3);
+        map.fitBounds(bounds);
+        overlay = L.imageOverlay.rotated(file.name, point1, point2, point3, {
+            opacity: 0.4,
+            interactive: true
+        });
 
 
 
-    function repositionImage() {
-      overlay.reposition(marker1.getLatLng(), marker2.getLatLng(), marker3.getLatLng());
+        marker1.on('drag dragend', repositionImage);
+        marker2.on('drag dragend', repositionImage);
+        marker3.on('drag dragend', repositionImage);
+
+        var switch_marker = 1;
+        map.addLayer(overlay);
+        overlay.on('dblclick', function(e) {
+            console.log('Double click on image.');
+            //cache les markers
+            if (switch_marker == 0) {
+
+                marker1 = L.marker(point1, {
+                    draggable: true
+                }).addTo(map);
+                marker2 = L.marker(point2, {
+                    draggable: true
+                }).addTo(map);
+                marker3 = L.marker(point3, {
+                    draggable: true
+                }).addTo(map);
+                marker1.on('drag dragend', repositionImage);
+                marker2.on('drag dragend', repositionImage);
+                marker3.on('drag dragend', repositionImage);
+                switch_marker = 1
+            } else {
+                map.removeLayer(marker1);
+                map.removeLayer(marker2);
+                map.removeLayer(marker3);
+                switch_marker = 0;
+            }
+
+            //e.stop();
+        });
+        overlay.on('click', function(e) {
+            console.log('Click on image.');
+        });
+
+
+
+        function repositionImage() {
+            overlay.reposition(marker1.getLatLng(), marker2.getLatLng(), marker3.getLatLng());
+        };
+
     };
-
-  };
-reader.readAsDataURL(file);
+    reader.readAsDataURL(file);
 
 
 }
 
 function setOverlayOpacity(opacity) {
-  overlay.setOpacity(opacity);
-}
-
-function trbl(top,right,bottom,left) {
-  return [[bottom,left],[top,right]];;
+    overlay.setOpacity(opacity);
 }
